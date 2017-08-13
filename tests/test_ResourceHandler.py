a9a5c86caac6bbdfdda0b29ab7e133a5493f4e82
CesarH <chernandez@vistaprint.com>
2017-08-12 20:11:45 -0400

    fixing test text and removing .cache folder

--- tests/test_ResourceHandler.py
+++ tests/test_ResourceHandler.py
@@ -2,7 +2,7 @@ from safelanguage.ResourceHandler import ResourceHandler
 
 
 def test_get_language_classifier():
-    sentence = ['Esto es una oracion en espanol y deberia ser predecido como es']
+    sentence = ['Esto es una oracion en español y deberia ser predecido como es']
     resource_handler = ResourceHandler()
     clf, target_names = resource_handler.get_language_classifier()
     prediction = clf.predict(sentence)
