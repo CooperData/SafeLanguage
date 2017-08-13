31619baf32fe61beaa76c5b70d507c17c26994ed
CesarH <chernandez@vistaprint.com>
2017-08-12 21:05:53 -0400

    Adding class to test resource file

--- tests/test_ResourceHandler.py
+++ tests/test_ResourceHandler.py
@@ -1,10 +1,12 @@
 from safelanguage.ResourceHandler import ResourceHandler
 
 
-def test_get_language_classifier():
-    sentence = ['Esto es una oracion en español y deberia ser predecido como es']
-    resource_handler = ResourceHandler()
-    clf, target_names = resource_handler.get_language_classifier()
-    prediction = clf.predict(sentence)
-    assert target_names[prediction[0]] == 'es'
+class TestResourceHandler(object):
+
+    def test_get_language_classifier(self):
+        sentence = ['Esto es una oracion en español y deberia ser predecido como es']
+        resource_handler = ResourceHandler()
+        clf, target_names = resource_handler.get_language_classifier()
+        prediction = clf.predict(sentence)
+        assert target_names[prediction[0]] == 'es'
 
