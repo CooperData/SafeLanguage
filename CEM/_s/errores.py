b6f7d86045b913aead9c4896f2fa5159a5186df6
AscanderS <ascander@gmail.com>
2017-08-17 11:05:40 -0400

    Use conda environment.

--- CEM/_s/errores.py
+++ CEM/_s/errores.py
@@ -1,5 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 # -*- coding: utf-8  -*-
+
+# conda execute
+# env:
+#  - python >=3
+
 import sys, getopt, re
 from operator import itemgetter
 import excluidos
